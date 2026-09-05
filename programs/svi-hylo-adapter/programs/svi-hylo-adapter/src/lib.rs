4a15919e9165e1dc70716a8c4457811b073e2a36
prince981620 <prince981620@gmail.com>
2026-09-05 14:01:36 +0545

    clean up and init svi-hylo-adapter

---
+++ programs/svi-hylo-adapter/programs/svi-hylo-adapter/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("FfK5xyE3v3GT2F9wzqhpLMHx7zCJsGPQrHAvxtgL3Mpr");
+
+#[program]
+pub mod svi_hylo_adapter {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
